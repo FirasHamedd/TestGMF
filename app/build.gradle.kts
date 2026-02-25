07baf3c0b8d7c2ad13be46dc3ea33cb134ebb659
Firas <firass.hamed@kindredgroup.com>
2026-02-25 15:08:01 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,29 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
     namespace = "com.example.testgmf"
+
+    packaging {
+        resources {
+            pickFirsts += setOf(
+                "META-INF/LICENSE.md",
+                "META-INF/LICENSE-notice.md"
+            )
+        }
+    }
+
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
         applicationId = "com.example.testgmf"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -47,20 +59,30 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    implementation(libs.bundles.compose)
     implementation(libs.bundles.retrofit)
+    implementation(libs.moshi)
+    implementation(libs.moshiAdapters)
+    ksp(libs.moshiCodegen)
     implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
     implementation(libs.bundles.coil)
+    // Junit5
+    testImplementation(platform(libs.junit5.bom))
+    testImplementation(libs.bundles.junit5Test)
+    testRuntimeOnly(libs.bundles.junit5Runtime)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+}
 
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
